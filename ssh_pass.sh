3f266279daa7635b8d5e2de43dc49fe8351eed11
q1ko <71758869+q1ko@users.noreply.github.com>
2024-01-06 00:56:39 +1100

    Update ssh_pass.sh

--- ssh_pass.sh
+++ ssh_pass.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-echo "This script will enable SSH password authentication."
-read -p "Do you want to proceed? (y/n): " choice
+read -p "Do you want to enable SSH password authentication? (y/n): " enable_password_auth
 
-if [ "$choice" = "y" ]; then
+read -p "Do you want to enable root login via password? (y/n): " enable_root_login
+
+if [ "$enable_password_auth" = "y" ]; then
+    echo "Enabling SSH password authentication..."
+    
     # Enable Password Authentication in SSH Config
     sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
-    
-    # Restart SSH Service
-    sudo systemctl restart sshd
-    
-    echo "SSH password authentication has been enabled."
-else
-    echo "SSH password authentication has NOT been modified."
 fi
+
+if [ "$enable_root_login" = "y" ]; then
+    echo "Enabling root login via password..."
+
+    # Enable Root Login with Password in SSH Config
+    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+fi
+
+# Restart SSH Service if changes were made
+if [ "$enable_password_auth" = "y" ] || [ "$enable_root_login" = "y" ]; then
+    sudo systemctl restart sshd
+    ec
