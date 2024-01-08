7aa4a9b1bf2d4119f2e91a75a535871a97a9650c
q1ko <71758869+q1ko@users.noreply.github.com>
2024-01-08 22:08:09 +1100

    Update choco.sh

--- choco.sh
+++ choco.sh
@@ -1,63 +1,59 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco source remove -n=chocolatey
-choco source add -n=nexus -s=http://192.168.1.152:8081/repository/chocolatey-group/
-
-choco install firefox
-choco install googlechrome
-choco install adobereader
-choco install zoom
-choco install python3
-choco install python
-choco install vlc
-choco install 7zip
-choco install notepadplusplus
-choco install git
-choco install paint.net
-choco install winrar
-choco install ccleaner
-choco install vscode
-choco install autohotkey.portable
-choco install wget
-choco install filezilla
-choco install slack
-choco install nodejs
-choco install curl
-choco install gimp
-choco install dropbox
-choco install thunderbird
-choco install spotify
-choco install jq
-choco install putty
-choco install itunes
-choco install vim
-choco install winscp
-choco install wireshark
-choco install onedrive
-choco install brave
-choco install cygwin
-choco install virtualbox
-choco install webex-meetings
-choco install windirstat
-choco install audacity
-choco install ffmpeg
-choco install docker-desktop
-choco install gradle
-choco install maven
-choco install signal
-choco install qbittorrent
-choco install openjdk
-choco install bitwarden
-choco install rufus
-choco install chocolatey-core.extension
-choco install git.install
-choco install googledrive
-choco install malwarebytes
-choco install obs-studio
-choco install streamlabs-obs
-choco install steam
-choco install minecraft-launcher
-choco install tabby
-choco install alacritty
-choco install cyberduck
-choco install intellijidea-community
-choco install pycharm-community
+choco install firefox -y
+choco install googlechrome -y
+choco install adobereader -y
+choco install zoom -y
+choco install python3 -y
+choco install python -y
+choco install vlc -y
+choco install 7zip -y
+choco install notepadplusplus -y
+choco install git -y
+choco install paint.net -y
+choco install winrar -y
+choco install ccleaner -y
+choco install vscode -y
+choco install autohotkey.portable -y
+choco install wget -y
+choco install filezilla -y
+choco install slack -y
+choco install nodejs -y
+choco install curl -y
+choco install gimp -y
+choco install dropbox -y
+choco install thunderbird -y
+choco install spotify -y
+choco install jq -y
+choco install putty -y
+choco install itunes -y
+choco install vim -y
+choco install winscp -y
+choco install wireshark -y
+choco install onedrive -y
+choco install brave -y
+choco install cygwin -y
+choco install virtualbox -y
+choco install webex-meetings -y
+choco install windirstat -y
+choco install audacity -y
+choco install ffmpeg -y
+choco install docker-desktop -y
+choco install gradle -y
+choco install maven -y
+choco install signal -y
+choco install qbittorrent -y
+choco install openjdk -y
+choco install bitwarden -y
+choco install rufus -y
+choco install chocolatey-core.extension -y
+choco install git.install -y
+choco install googledrive -y
+choco install malwarebytes -y
+choco install obs-studio -y
+choco install streamlabs-obs -y
+choco install steam -y
+choco install minecraft-launcher -y
+choco install tabby -y
+choco install alacritty -y
+choco install cyberduck -y
+choco install intellijidea-community -y
+choco install pycharm-community -y
