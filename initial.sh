a3db666185dcf59e2bb074ae59812f795fab2f4c
q1ko <71758869+q1ko@users.noreply.github.com>
2024-03-06 15:00:06 +1100

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,3 +1,3 @@
 echo 'Acquire::http { Proxy "http://192.168.1.150:3142"; }' | sudo tee -a /etc/apt/apt.conf.d/proxy
 sudo apt update && sudo apt upgrade -y
-sudo apt install net-tools
+sudo apt install net-tools curl
