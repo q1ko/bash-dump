0654f8ea565405197259a12d2b64b26c861bf5b2
q1ko <71758869+q1ko@users.noreply.github.com>
2024-03-07 19:26:38 +1100

    Update teleport.sh

--- teleport.sh
+++ teleport.sh
@@ -6,10 +6,9 @@ read -p "Enter the username for the Teleport user: " teleport_username
 # Prompt for FQDN
 read -p "Enter the FQDN for the Teleport cluster: " teleport_fqdn
 
-echo "$teleport_fqdn $teleport_username"
-
 curl https://goteleport.com/static/install.sh | bash -s 15.1.1
-apt install systemctl sudo openssl -y
+
+apt install systemctl openssl tctl -y
 
 # Define variables
 teleport_host_privkey="/var/lib/teleport/privkey.pem"
@@ -19,18 +18,20 @@ teleport_public_addr="$teleport_fqdn:443"
 
 # Generate private key if not exists
 if [ ! -f "$teleport_host_privkey" ]; then
-    sudo openssl genpkey -algorithm RSA -out "$teleport_host_privkey" 
+    openssl genpkey -algorithm RSA -out "$teleport_host_privkey" 
 fi
 
 # Generate certificate if not exists
 if [ ! -f "$teleport_host_fullchain" ]; then
-    sudo openssl req -new -key "$teleport_host_privkey" -x509 -days 365 -out "$teleport_host_fullchain" -subj "/CN=*.${teleport_fqdn}" 
+    openssl req -new -key "$teleport_host_privkey" -x509 -days 365 -out "$teleport_host_fullchain" -subj "/CN=*.${teleport_fqdn}" 
 fi
 
 # Run teleport configure command
-sudo teleport configure -o file \
+teleport configure -o file \
     --cluster-name="$teleport_cluster_name" \
     --public-addr="$teleport_public_addr" \
     --cert-file="$teleport_host_fullchain" \
     --key-file="$teleport_host_privkey" 
 
+systemctl enable teleport
+systemctl start teleport
